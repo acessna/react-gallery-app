2a4ecfef3878ccf582912558fd2d5c04b0eceb62
Adam Cessna <racessna@gmail.com>
2020-10-07 15:58:30 -0500

    Update routes for stock categories. Remove state from PhotoContainer and have App handle state. Start search form component

--- src/Components/App.js
+++ src/Components/App.js
@@ -2,18 +2,62 @@ import React, { Component } from 'react';
 import { Route, BrowserRouter } from 'react-router-dom';
 import Nav from './Nav';
 import PhotoContainer from './PhotoContainer';
+import SearchForm from './SearchForm';
+import axios from 'axios';
+import apiKey from '../config';
 
 
 
 export default class App extends Component {
 
+    state = {
+      imgs: [],
+      Cars: [],
+      Houses: [],
+      Birds: []
+    }
+
+
+
+
+  getImages = (query) => {
+    axios.get(`https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&tags=${query}&text=${query}&per_page=24&format=json&nojsoncallback=1`)
+    .then(response => {
+        this.setState({imgs: response.data.photos.photo});
+    })
+     .catch(error => {
+       console.log('Error fetching and parsing data', error);
+     });
+  }
+
+  getCategory = (query) => {
+    axios.get(`https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&tags=${query}&text=${query}&per_page=24&format=json&nojsoncallback=1`)
+    .then(response => {
+      this.setState({[query]: response.data.photos.photo});
+    })
+     .catch(error => {
+       console.log('Error fetching and parsing data', error);
+     });
+  }
+      componentDidMount(){
+        this.getCategory("Cars");
+        this.getCategory("Houses");
+        this.getCategory("Birds");
+    }
+
   render(){
   return (
     <BrowserRouter>
       <div className="container">
+        <SearchForm onSearch={this.getImages}/>
         <Nav />
-        <Route exact path='/:category' component = {PhotoContainer}/>
+        <Route path="/search/:query" render ={() => <PhotoContainer data={this.state.imgs}/>}/>
+        <Route path="/Cars" render = {() => <PhotoContainer data={this.state.Cars}/>}/>
+        <Route path="/Houses" render = {() => <PhotoContainer data={this.state.Houses}/>}/>
+        <Route path="/Birds" render = {() => <PhotoContainer data={this.state.Birds}/>}/>
       </div>
+
+
     </BrowserRouter>
   );
   }
--- src/Components/PhotoContainer.js
+++ src/Components/PhotoContainer.js
@@ -1,33 +1,10 @@
-import React, { Component } from 'react'; 
+import React from 'react'; 
 import Photo from './Photo';
-import axios from 'axios';
-import apiKey from '../config';
 import NotFound from './NotFound';
 
-export default class PhotoContainer extends Component {
-
-    constructor(props) {
-        super(props);
-        this.state = {
-          imgs: [],
-        }
-      }
-    
-      getImages = (query) => {
-        axios.get(`https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&tags=${query}&text=${query}&per_page=24&format=json&nojsoncallback=1`)
-        .then(response => {
-            this.setState({imgs: response.data.photos.photo});
-        })
-         .catch(error => {
-           console.log('Error fetching and parsing data', error);
-         });
-      }
-
-      render(){
-
-        this.getImages(this.props.match.params.category);
+const PhotoContainer = props => {
         let imgs;
-        const results = this.state.imgs;
+        const results = props.data;
         if(results.length > 0){
             imgs = results.map(img => <Photo url={`https://live.staticflickr.com/${img.server}/${img.id}_${img.secret}.jpg`} key={img.id}/>)
         } else {
@@ -41,7 +18,8 @@ export default class PhotoContainer extends Component {
                 </ul>
             </div>
           );
-      }
 
 }
+
+export default PhotoContainer;
 
