eed177ca9432305485e1a83663e5b7c174cc30ac
Adam Cessna <racessna@gmail.com>
2020-10-15 15:27:03 -0500

    fetch data in app component and refactor photo container

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,14 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Route, BrowserRouter, Redirect, Switch } from 'react-router-dom';
 import Nav from './Nav';
 import PhotoContainer from './PhotoContainer';
 import SearchForm from './SearchForm';
 import NotFound from './NotFound';
+import axios from 'axios';
+import apiKey from '../config';
 
 
+export default class App extends Component {
 
-const App = () => {
+  state ={
+    imgs: []
+  }
 
+  getImages = (query) => {
+    axios.get(`https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&tags=${query}&text=${query}&per_page=24&format=json&nojsoncallback=1`)
+    .then(response => {
+        this.setState({imgs: response.data.photos.photo});
+    })
+     .catch(error => {
+       console.log('Error fetching and parsing data', error);
+     });
+  }
+
+
+  render(){
   return (
     <BrowserRouter>
       <div className="container">
@@ -18,8 +35,8 @@ const App = () => {
           <Route exact path="/">
           <Redirect to="/home/Cars" />
           </Route>
-          <Route exact path="/search/:query" component={PhotoContainer}/>
-          <Route  exact path="/home/:query" component={PhotoContainer}/>
+          <Route exact path="/search/:query" render= { routeProps => <PhotoContainer {...routeProps} data={this.state.imgs} getImages={this.getImages}/>}/>
+          <Route  exact path="/home/:query" render= {routeProps => <PhotoContainer {...routeProps} data={this.state.imgs} getImages={this.getImages}/>}/>
           <Route>
             <NotFound />
           </Route>
@@ -27,7 +44,6 @@ const App = () => {
       </div>
     </BrowserRouter>
   );
+  }
 }
-
-export default App;
 
--- src/Components/PhotoContainer.js
+++ src/Components/PhotoContainer.js
@@ -1,49 +1,47 @@
 import React, { Component } from 'react'; 
 import Photo from './Photo';
 import NotFound from './NotFound';
-import axios from 'axios';
-import apiKey from '../config';
 
 export default class PhotoContainer extends Component {
-    constructor(props) {
+
+    constructor(props){
         super(props);
-        this.state = {
-            imgs: []
+        this.state={
+            imgs: ""
         }
     }
-    
-    getImages = (query) => {
-        axios.get(`https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&tags=${query}&text=${query}&per_page=24&format=json&nojsoncallback=1`)
-        .then(response => {
-            this.setState({imgs: response.data.photos.photo});
-        })
-         .catch(error => {
-           console.log('Error fetching and parsing data', error);
-         });
-      }
 
+        componentDidMount(){
+            this.props.getImages(this.props.match.params.query);
+            let results = this.props.data;
+            let imgs;
+            if(results.length > 0){
+                    imgs = results.map(img => <Photo url={`https://live.staticflickr.com/${img.server}/${img.id}_${img.secret}.jpg`} key={img.id}/>)
+            } else {
+                    imgs= <NotFound />
+            }
 
+            this.setState({
+                imgs:imgs
+            });
+        }
 
         render(){
-        this.getImages(this.props.match.params.query)
-        let imgs;
-        const results = this.state.imgs;
-        let title = this.props.match.params.query;
-        if(results.length > 0){
-            imgs = results.map(img => <Photo url={`https://live.staticflickr.com/${img.server}/${img.id}_${img.secret}.jpg`} key={img.id}/>)
-        } else {
-            imgs = <NotFound />
-        }
+
+
+
+            console.log(this.state.imgs);
+
           return(
             <div className="photo-container">
-                <h2>{title}</h2>
+                <h2>{this.props.match.params.query}</h2>
                 <ul>
-                    {imgs}
+                    {this.state.imgs}
                 </ul>
             </div>
           );
-
     }
 }
+
 
 
