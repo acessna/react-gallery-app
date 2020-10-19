af1e15b1b3963640c1d0187a3d8edb80ad824f05
Adam Cessna <racessna@gmail.com>
2020-10-19 15:31:37 -0500

    make PhotoContainer a class and write getPhotos function

--- src/Components/PhotoContainer.js
+++ src/Components/PhotoContainer.js
@@ -1,28 +1,42 @@
-import React from 'react'; 
+import React, { Component } from 'react'; 
 import Photo from './Photo';
 import NotFound from './NotFound';
 
-const PhotoContainer = (props) => {
-
-    let results = props.data;
-    let imgs;
-    if(results){
-            imgs = results.map(img => <Photo url={`https://live.staticflickr.com/${img.server}/${img.id}_${img.secret}.jpg`} key={img.id}/>)
-    } else {
-            imgs= <NotFound />
+export default class PhotoContainer extends Component {
+    constructor(props){
+        super(props);
+        this.state={
+            photos: ""
+        }
     }
-    return(
-    <div className="photo-container">
-        <h2>{props.match.params.query}</h2>
-        <ul>
-            {imgs}
-        </ul>
-    </div>
-    );
+    
+    getPhotos(){
+        let results = this.props.data;
+        let imgs;
+        if(results.length > 0){
+                imgs = results.map(img => <Photo url={`https://live.staticflickr.com/${img.server}/${img.id}_${img.secret}.jpg`} key={img.id}/>)
+        } else {
+                imgs= <NotFound />
+        }
+
+        this.setState({photos:imgs});
+        console.log(this.state.photos);
+    }
+
+    render(){
+        this.getPhotos();
+        return(
+        <div className="photo-container">
+            <h2>{this.props.match.params.query}</h2>
+            <ul>
+                {this.state.photos}
+            </ul>
+        </div>
+        );
+    }
     
 }
 
-export default PhotoContainer;
 
 
 
